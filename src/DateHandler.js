2d6b6c0bb24aaaaa002d6de972825809406e4404
joel-jcs <joeljcs@protonmail.com>
2024-09-25 18:40:08 -0400

    added 12 hour option

--- src/DateHandler.js
+++ src/DateHandler.js
@@ -18,6 +18,7 @@ const DateHandler = () => {
       hour: '2-digit',
       minute: '2-digit',
       second: '2-digit',
+      hour12: true,
     };
 
     const formatter = new Intl.DateTimeFormat('en-US', options);
