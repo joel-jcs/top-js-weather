ff59e660c927d0952afce3d9c36f14bbd1599e7d
joel-jcs <joeljcs@protonmail.com>
2024-09-25 23:38:07 -0400

    set initializer to search box. removed unneeded imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './styles.css';
-import WeatherService from './WeatherService.js';
-import DateHandler from './DateHandler.js';
 import DOMHandler from './DOMHandler.js';
 
-DOMHandler.loadContent();
+DOMHandler.initDOM();
