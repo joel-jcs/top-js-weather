399d651256f9f48bb37ea6a3bbaaead364d169f8
joel-jcs <joeljcs@protonmail.com>
2024-09-25 23:32:06 -0400

    refactored to only call weatherService API once. This was achieved by adding a data parameter to each function, which is only obtained after the await on the initialization of that variable. also added initDOM which sets the initial searchbox

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -2,6 +2,39 @@ import WeatherService from './WeatherService';
 import DateHandler from './DateHandler';
 
 const DOMHandler = () => {
+  const initDOM = () => {
+    const contentContainer = document.getElementById('content-container');
+    const mainContainer = document.createElement('div');
+    mainContainer.id = 'main-container';
+
+    const searchBox = document.createElement('div');
+    searchBox.id = 'search-box';
+    searchBox.classList.add('main-container-card');
+
+    const searchForm = document.createElement('form');
+    searchForm.id = 'search-form';
+
+    const searchInput = document.createElement('input');
+    searchInput.id = 'search-input';
+    searchInput.type = 'text';
+    searchInput.placeholder = 'Search for a city';
+
+    const searchBtn = document.createElement('button');
+    searchBtn.id = 'search-btn';
+    searchBtn.type = 'button';
+    searchBtn.innerText = 'Search';
+
+    searchForm.append(searchInput, searchBtn);
+    searchBox.append(searchForm);
+    mainContainer.append(searchBox);
+    contentContainer.append(mainContainer);
+
+    searchBtn.onclick = (e) => {
+      e.preventDefault();
+      loadContent();
+    };
+  };
+
   const getWeatherIcon = (timeframeIcon) => {
     const clearIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>weather-sunny</title><path d="M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,2L14.39,5.42C13.65,5.15 12.84,5 12,5C11.16,5 10.35,5.15 9.61,5.42L12,2M3.34,7L7.5,6.65C6.9,7.16 6.36,7.78 5.94,8.5C5.5,9.24 5.25,10 5.11,10.79L3.34,7M3.36,17L5.12,13.23C5.26,14 5.53,14.78 5.95,15.5C6.37,16.24 6.91,16.86 7.5,17.37L3.36,17M20.65,7L18.88,10.79C18.74,10 18.47,9.23 18.05,8.5C17.63,7.78 17.1,7.15 16.5,6.64L20.65,7M20.64,17L16.5,17.36C17.09,16.85 17.62,16.22 18.04,15.5C18.46,14.77 18.73,14 18.87,13.21L20.64,17M12,22L9.59,18.56C10.33,18.83 11.14,19 12,19C12.82,19 13.63,18.83 14.37,18.56L12,22Z" /></svg>`;
     const rainIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>weather-pouring</title><path d="M9,12C9.53,12.14 9.85,12.69 9.71,13.22L8.41,18.05C8.27,18.59 7.72,18.9 7.19,18.76C6.65,18.62 6.34,18.07 6.5,17.54L7.78,12.71C7.92,12.17 8.47,11.86 9,12M13,12C13.53,12.14 13.85,12.69 13.71,13.22L11.64,20.95C11.5,21.5 10.95,21.8 10.41,21.66C9.88,21.5 9.56,20.97 9.7,20.43L11.78,12.71C11.92,12.17 12.47,11.86 13,12M17,12C17.53,12.14 17.85,12.69 17.71,13.22L16.41,18.05C16.27,18.59 15.72,18.9 15.19,18.76C14.65,18.62 14.34,18.07 14.5,17.54L15.78,12.71C15.92,12.17 16.47,11.86 17,12M17,10V9A5,5 0 0,0 12,4C9.5,4 7.45,5.82 7.06,8.19C6.73,8.07 6.37,8 6,8A3,3 0 0,0 3,11C3,12.11 3.6,13.08 4.5,13.6V13.59C5,13.87 5.14,14.5 4.87,14.96C4.59,15.43 4,15.6 3.5,15.32V15.33C2,14.47 1,12.85 1,11A5,5 0 0,1 6,6C7,3.65 9.3,2 12,2C15.43,2 18.24,4.66 18.5,8.03L19,8A4,4 0 0,1 23,12C23,13.5 22.2,14.77 21,15.46V15.46C20.5,15.73 19.91,15.57 19.63,15.09C19.36,14.61 19.5,14 20,13.72V13.73C20.6,13.39 21,12.74 21,12A2,2 0 0,0 19,10H17Z" /></svg>`;
@@ -25,10 +58,9 @@ const DOMHandler = () => {
     return weatherIcon;
   };
 
-  const renderLocation = async () => {
-    const data = await WeatherService.getWeatherData();
-    const tz = await DateHandler.getTimezone();
-    const dateAndTime = await DateHandler.getFormattedDate(tz);
+  const renderLocation = (data) => {
+    const tz = DateHandler.getTimezone(data);
+    const dateAndTime = DateHandler.getFormattedDate(tz);
 
     const locationContainer = document.createElement('div');
     locationContainer.id = 'location-container';
@@ -47,8 +79,7 @@ const DOMHandler = () => {
     return locationContainer;
   };
 
-  const renderWeatherSummary = async () => {
-    const data = await WeatherService.getWeatherData();
+  const renderWeatherSummary = (data) => {
     const summary = data.today.description;
 
     const weatherSummaryContainer = document.createElement('div');
@@ -67,8 +98,7 @@ const DOMHandler = () => {
     return weatherSummaryContainer;
   };
 
-  const renderCurrentWeather = async () => {
-    const data = await WeatherService.getWeatherData();
+  const renderCurrentWeather = (data) => {
     const currWeather = data.today;
 
     const currentWeatherContainer = document.createElement('div');
@@ -150,10 +180,9 @@ const DOMHandler = () => {
     return currentWeatherContainer;
   };
 
-  const renderHourlyWeather = async () => {
-    const data = await WeatherService.getWeatherData();
+  const renderHourlyWeather = (data) => {
     const { todayHourlyForecast, tomorrowHourlyForecast } = data;
-    const currentHour = await DateHandler.getCurrentHour();
+    const currentHour = DateHandler.getCurrentHour(data);
 
     const hourlyWeatherContainer = document.createElement('div');
     hourlyWeatherContainer.id = 'hourly-weather-container';
@@ -161,7 +190,7 @@ const DOMHandler = () => {
 
     const hourlyWeatherHeading = document.createElement('h2');
     hourlyWeatherHeading.id = 'hourly-weather-heading';
-    hourlyWeatherHeading.textContent = 'Hourly Weather';
+    hourlyWeatherHeading.textContent = 'Hourly Forecast (24H)';
 
     const hourlyDataContainer = document.createElement('div');
     hourlyDataContainer.id = 'hourly-data-container';
@@ -193,6 +222,18 @@ const DOMHandler = () => {
             `;
           hourlyDataContainer.append(hourlyWeatherCard);
         });
+      } else {
+        // TO-DO -> DRY
+        const weatherIcon = getWeatherIcon(hour.icon);
+
+        const hourlyWeatherCard = document.createElement('div');
+        hourlyWeatherCard.classList.add('hourly-weather-card');
+        hourlyWeatherCard.innerHTML += `
+          <h3>${trimmedHour}</h3>
+          ${weatherIcon}
+          <p>${hour.temp.toFixed(0)}°</p>
+        `;
+        hourlyDataContainer.append(hourlyWeatherCard);
       }
     });
 
@@ -201,8 +242,7 @@ const DOMHandler = () => {
     return hourlyWeatherContainer;
   };
 
-  const renderDailyWeather = async () => {
-    const data = await WeatherService.getWeatherData();
+  const renderDailyWeather = (data) => {
     const dailyData = data.dailyForecast;
 
     const dailyWeatherContainer = document.createElement('div');
@@ -216,7 +256,7 @@ const DOMHandler = () => {
     const dailyDataContainer = document.createElement('div');
     dailyDataContainer.id = 'daily-data-container';
 
-    dailyData.forEach(async (day, index) => {
+    dailyData.forEach((day, index) => {
       const dailyWeatherCard = document.createElement('div');
       dailyWeatherCard.classList.add('daily-weather-card');
 
@@ -225,11 +265,9 @@ const DOMHandler = () => {
 
       let dayName;
       if (index === 0) {
-        dayName = 'Today';
-      } else if (index === 1) {
         dayName = 'Tomorrow';
       } else {
-        const dayOfWeek = await DateHandler.getDayOfWeek(day.datetime);
+        const dayOfWeek = DateHandler.getDayOfWeek(day.datetime);
         dayName = dayOfWeek;
       }
 
@@ -290,8 +328,7 @@ const DOMHandler = () => {
     return dailyWeatherContainer;
   };
 
-  const renderDaylight = async () => {
-    const data = await WeatherService.getWeatherData();
+  const renderDaylight = (data) => {
     const { sunrise, sunset } = data.today;
 
     const daylightContainer = document.createElement('div');
@@ -321,18 +358,18 @@ const DOMHandler = () => {
   };
 
   const loadContent = async () => {
-    const mainCardContainer = document.createElement('div');
-    mainCardContainer.id = 'main-container';
+    const data = await WeatherService.getWeatherData();
 
-    const locationContainer = await renderLocation();
+    const mainContainer = document.getElementById('main-container');
 
-    const weatherSummaryContainer = await renderWeatherSummary();
-    const currentWeatherContainer = await renderCurrentWeather();
-    const hourlyWeatherContainer = await renderHourlyWeather();
-    const dailyWeatherContainer = await renderDailyWeather();
-    const sunsetSunriseContainer = await renderDaylight();
+    const locationContainer = renderLocation(data);
+    const weatherSummaryContainer = renderWeatherSummary(data);
+    const currentWeatherContainer = renderCurrentWeather(data);
+    const hourlyWeatherContainer = renderHourlyWeather(data);
+    const dailyWeatherContainer = renderDailyWeather(data);
+    const sunsetSunriseContainer = renderDaylight(data);
 
-    mainCardContainer.append(
+    mainContainer.append(
       locationContainer,
       weatherSummaryContainer,
       currentWeatherContainer,
@@ -341,13 +378,10 @@ const DOMHandler = () => {
       sunsetSunriseContainer,
     );
 
-    const contentContainer = document.getElementById('content-container');
-    contentContainer.append(mainCardContainer);
-
-    return contentContainer;
+    return mainContainer;
   };
 
-  return { loadContent };
+  return { initDOM, loadContent };
 };
 
 export default DOMHandler();
